ff3defa7e4f20b6cd400574b6eb8920621e5aa1e
Victor <victornjoseph@gmail.com>
2019-06-21 15:08:55 +0300

    Refactored scraping scripts

--- jobapp/views.py
+++ jobapp/views.py
@@ -3,6 +3,10 @@ from django.http import JsonResponse
 from bs4 import BeautifulSoup
 import requests
 import json
+from threading import Thread
+from multiprocessing import Process
+import os
+import time
 
 
 def homeView(request):
@@ -13,7 +17,7 @@ def homeView(request):
     except requests.exceptions as err:
         print(err)
     else:
-        soup = BeautifulSoup(response.text, "html.parser")
+        soup = BeautifulSoup(response.text, "lxml")
         jobs_list = soup.find_all("div", {"class":"jobsboard-row"})
 
         for job in jobs_list:
@@ -33,59 +37,96 @@ def homeView(request):
 
 
 def search(request):
-    keyword = ("-").join(request.GET["keyword"].split(" "))
-    # keyword = "software-developer"
+    # keyword = ("-").join(request.GET["keyword"].split(" "))
+    start_time = time.time()
+    keyword = "software-developer"
     response_dict = {}
 
-    url = "https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword={}&sc.keyword={}&locT=N&locId=130&jobType=".format(keyword,keyword)
-    headers = {
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/  74.0.3729.169 Chrome74.0.3729.169 Safari/537.36',
-    }
+    def glassdoor():
 
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, "html.parser")
-    jobs = soup.find_all("li",{"class":"jl"})
-    print(url)
-    glass_dict = {}
-    glass_id = 1
-    for job in jobs:
-        # print(job.find("div",{"class":"jobEmpolyerName"}))
-        company = job.find("div",{"class" : "jobEmpolyerName"}).text
-        job_title = job.find("div",{"class":"jobContainer"}).a.text
-        job_link = "https://www.glassdoor.com{}".format(job.find("div",{"class":"jobContainer"}).a["href"])
-        glass_dict[glass_id] = {"company":company, "job_title":job_title, "job_link":job_link}
-        glass_id = glass_id + 1
+        url = "https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword={}&sc.keyword={}&locT=N&locId=130&jobType=".format(keyword,keyword)
+        headers = {
+            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/  74.0.3729.169 Chrome74.0.3729.169 Safari/537.36',
+        }
 
-    response_dict["glassdoor"] = glass_dict
-    response_dict["glassdoor_count"] = glass_id
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, "lxml")
+        jobs = soup.find_all("li",{"class":"jl"})
+        # print(url)
+        glass_dict = {}
+        glass_id = 1
+        for job in jobs:
+            # print(job.find("div",{"class":"jobEmpolyerName"}))
+            company = job.find("div",{"class" : "jobEmpolyerName"}).text
+            job_title = job.find("div",{"class":"jobContainer"}).a.text
+            job_link = "https://www.glassdoor.com{}".format(job.find("div",{"class":"jobContainer"}).a["href"])
+            glass_dict[glass_id] = {"company":company, "job_title":job_title, "job_link":job_link}
+            glass_id = glass_id + 1
+
+        response_dict["glassdoor"] = glass_dict
+        # response_dict["glassdoor_count"] = glass_id
     
-    try:
-        ihub_response = requests.get("https://ihub.co.ke/jobs")
-    except requests.exceptions as err:
-        print(err)
-    else:
-        id = 1
-        ihub_soup = BeautifulSoup(ihub_response.text, "html.parser")
-        jobs_list = ihub_soup.find_all("div", {"class":"jobsboard-row"})
-        hub_dict = {}
-        for job in jobs_list:
-            job_title = list(job.find_all("a"))[1].text
-            company = job.find("div", {"class":"job-company"}).text
-            job_more = "https://ihub.co.ke{}".format(job.find("a", {"class":"job-more"})["href"])
-            description = job.find("div", {"class":"post-description"}).find("a").text
-            # hub_dict.update({"company_name":company, "job_title":job_title,"description":description,   "job_details":job_more})
-            hub_dict[id] = {"company_name":company, "job_title":job_title,"description":description,  "job_details":job_more}
-            id += 1
-        
-        response_dict["ihub"] = hub_dict
-        response_dict["ihub_count"] = id
+    
+    def ihub():
+        try:
+            ihub_response = requests.get("https://ihub.co.ke/jobs")
+        except requests.exceptions as err:
+            print(err)
+        else:
+            id = 1
+            ihub_soup = BeautifulSoup(ihub_response.text, "lxml")
+            jobs_list = ihub_soup.find_all("div", {"class":"jobsboard-row"})
+            hub_dict = {}
+            for job in jobs_list:
+                job_title = list(job.find_all("a"))[1].text
+                company = job.find("div", {"class":"job-company"}).text
+                job_more = "https://ihub.co.ke{}".format(job.find("a", {"class":"job-more"})["href"])
+                description = job.find("div", {"class":"post-description"}).find("a").text
+                # hub_dict.update({"company_name":company, "job_title":job_title,"description":description,   "job_details":job_more})
+                hub_dict[id] = {"company_name":company, "job_title":job_title,"description":description,  "job_details":job_more}
+                id += 1
+            
+            response_dict["ihub"] = hub_dict
+            # response_dict["ihub_count"] = id
+
+    # p = Pool(10)  # Pool tells how many at a time
+    # glassdoor_process = p.map(parse, glassdoor)
+    # ihub_process = p.map(parse, ihub)
+    # p.terminate()
+    # p.join()
+
+    threads = []
+
+    for i in range(os.cpu_count()):
+        threads.append(Thread(target=glassdoor))
+        threads.append(Thread(target=ihub))
+
+    # print(threads)
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
 
 
+    print(time.time() - start_time)
     return JsonResponse(response_dict)
 
 
-def job_info(request):
-    return JsonResponse(data)
+
+
+
+
+
+
+
+
+
+
+
+
+# def job_info(request):
+#     return JsonResponse(data)
 
 
 def ajax(request):
